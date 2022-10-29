30790ad2c906d413c59e79774738154e40977902
yazbikod <aslan_murathan@hotmail.com>
2022-10-29 18:58:56 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int[] randomDizi = new int[10];               // integer tipinde 10 adetlik bir dizi oluşturalım
+int randomDeger;                             // oluşturacağımız random sayıları ilk olarak nu değişkene yazacağız
+Random random = new Random();               // random nesnesi oluşturarak bunu kullanacağız
+for (int i = 0; i < randomDizi.Length; i++)
+{
+    randomDeger=random.Next(100);
+    randomDizi[i] = randomDeger;                // oluşturulan random değerleri dizi içine atıyoruz
+    Console.WriteLine($"[{randomDizi[i]}]");  // dizimizi yazdırıyoruz
+
+}
